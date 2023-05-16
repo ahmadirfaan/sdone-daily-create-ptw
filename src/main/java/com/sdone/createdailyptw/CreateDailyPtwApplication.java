d4002cd4280d1d45c8c0f02f3ecf2dca9a8abdd8
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2023-05-16 18:58:12 +0700

    create template project

---
+++ src/main/java/com/sdone/createdailyptw/CreateDailyPtwApplication.java
@@ -0,0 +1,12 @@
+package com.sdone.createdailyptw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateDailyPtwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateDailyPtwApplication.class, args);
+    }
+}
